c34981142a6160a199648c8aa84c5d16b6fbde42
renyu <Yuren2@hotmail.com>
2017-03-15 22:21:50 +0800

    添加雇员列表

---
+++ app/src/main/java/com/renyu/sostar/activity/order/EmployeeListActivity.java
@@ -0,0 +1,37 @@
+package com.renyu.sostar.activity.order;
+
+import android.graphics.Color;
+
+import com.renyu.commonlibrary.baseact.BaseActivity;
+import com.renyu.sostar.R;
+
+/**
+ * Created by renyu on 2017/3/15.
+ */
+
+public class EmployeeListActivity extends BaseActivity {
+    @Override
+    public void initParams() {
+
+    }
+
+    @Override
+    public int initViews() {
+        return R.layout.activity_employeelist;
+    }
+
+    @Override
+    public void loadData() {
+
+    }
+
+    @Override
+    public int setStatusBarColor() {
+        return Color.WHITE;
+    }
+
+    @Override
+    public int setStatusBarTranslucent() {
+        return 0;
+    }
+}
