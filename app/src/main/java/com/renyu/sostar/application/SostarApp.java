6f020f8a91a3d34c658eeb7154efcfb0b9bca90d
renyu <Yuren2@hotmail.com>
2017-05-31 16:57:51 +0800

    调整fresco初始化

--- app/src/main/java/com/renyu/sostar/application/SostarApp.java
+++ app/src/main/java/com/renyu/sostar/application/SostarApp.java
@@ -6,21 +6,14 @@ import android.support.multidex.MultiDexApplication;
 
 import com.alipay.euler.andfix.patch.PatchManager;
 import com.baidu.mapapi.SDKInitializer;
-import com.facebook.cache.disk.DiskCacheConfig;
-import com.facebook.common.disk.NoOpDiskTrimmableRegistry;
-import com.facebook.common.internal.Supplier;
 import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.imagepipeline.cache.MemoryCacheParams;
-import com.facebook.imagepipeline.core.ImagePipelineConfig;
 import com.meituan.android.walle.WalleChannelReader;
 import com.renyu.commonlibrary.commonutils.Utils;
 import com.renyu.commonlibrary.network.Retrofit2Utils;
-import com.renyu.commonlibrary.params.InitParams;
+import com.renyu.imagelibrary.commonutils.ImagePipelineConfigUtils;
 import com.renyu.sostar.BuildConfig;
 import com.renyu.sostar.params.CommonParams;
 import com.tencent.bugly.crashreport.CrashReport;
-
-import java.io.File;
 
 import cn.jpush.android.api.JPushInterface;
 
@@ -55,22 +48,7 @@ public class SostarApp extends MultiDexApplication {
             CrashReport.initCrashReport(this, CommonParams.BUGLY_APPID, true, strategy);
 
             // 初始化fresco
-            DiskCacheConfig diskCacheConfig = DiskCacheConfig.newBuilder(this)
-                    // 缓存文件目录
-                    .setBaseDirectoryPath(new File(InitParams.CACHE_PATH))
-                    // 缓存文件夹名
-                    .setBaseDirectoryName(InitParams.FRESCO_CACHE_NAME)
-                    .setDiskTrimmableRegistry(NoOpDiskTrimmableRegistry.getInstance())
-                    .build();
-            Fresco.initialize(this, ImagePipelineConfig.newBuilder(this).setMainDiskCacheConfig(diskCacheConfig)
-                    .setBitmapMemoryCacheParamsSupplier(() -> new MemoryCacheParams(
-                            (int) Runtime.getRuntime().maxMemory() / 5, // 内存缓存中总图片的最大大小,以字节为单位。
-                            Integer.MAX_VALUE, // 内存缓存中图片的最大数量。
-                            (int) Runtime.getRuntime().maxMemory() / 5,  // 内存缓存中准备清除但尚未被删除的总图片的最大大小,以字节为单位。
-                            Integer.MAX_VALUE, // 内存缓存中准备清除的总图片的最大数量。
-                            Integer.MAX_VALUE)) // 内存缓存中单个图片的最大大小。
-                    .setDownsampleEnabled(true)
-                    .build());
+            Fresco.initialize(this, ImagePipelineConfigUtils.getDefaultImagePipelineConfig(this));
 
             // AndFix初始化
             mPatchManager = new PatchManager(this);
